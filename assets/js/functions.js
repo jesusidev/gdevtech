35878ab9f5653d0ffecde515f49ed8f7215edcfa
gdevtech <jesus@guzman1.com>
2015-12-10 22:33:29 -0700

    Adding full directory
    Adding full directory structure including Stylus and Jade. Also added Git
    Ignore file.

---
+++ assets/js/functions.js
@@ -0,0 +1,3 @@
+$("#typed").typed({
+    stringsElement: $('#typed-strings')
+});
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+//	Include gulp
+var gulp = require('gulp');
+
+// Include Plugins
+var stylus = require('gulp-stylus');
+var jade = require('gulp-jade');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Compile Stylus
+gulp.task('styles', function(){
+	return gulp.src('assets/css/main.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('assets/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function(){
+	return gulp.src('assets/js/*.js')
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('assets/js'))
+		.pipe(rename('main.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('assets/js'))
+});
+
+// Compile Jade
+gulp.task('templates', function() {
+  return gulp.src('views/index.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function(){
+	gulp.watch('assets/js/*.js',['scripts']);
+	gulp.watch([
+		'assets/css/main.styl',
+		'assets/css/_1tools/*.styl',
+		'assets/css/_2basics/*.styl',
+		'assets/css/_4pages/*.styl'
+		],['styles']);
+	gulp.watch([
+		'views/index.jade',
+		'views/partials/*.jade'
+		],['templates']);
+});
+
+// Default Task
+gulp.task('default', ['styles', 'scripts', 'templates',
+	'watch']);
