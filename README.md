c60731fbfbdc1038bf521b2d930b5440a7e4e870
gdevtech <jesus@guzman1.com>
2016-02-09 20:37:37 -0700

    Finished Recent Works and Contact Section

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Website for gdevtech.co
 
-## Want to see what it will look like when its finish? Then look below...
-
-![G Dev Tech](http://gdevtech.co/assets/img/GDevTech.jpg)
-
 ##Big shout out to Mike who provide the PSD for free here http://www.yoarts.com/mike-free-single-page-website-psd/
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -43,8 +43,37 @@ $(function() {
             }
         });
     });
-     $('#myTabs a').click(function (e) {
-       e.preventDefault();
-      $(this).tab('show');
-     });
+    $('#myTabs a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    });
+
+    $('.thumbnail').click(function(evt){
+    evt.preventDefault();
+    $('.modal-body').empty();
+    var title = $(this).parent('a').attr("title");
+    $('.modal-title').html(title);
+    $($(this).parents('div').html()).appendTo('.modal-body');
+    $('#myModal').modal({show:true});
+    });
+
+      $('#name').keyup(function(){
+      $('.name').addClass('typing');
+      if( $(this).val().length == 0 ) {
+          $('.name').removeClass('typing');
+      }
+    });
+    $('#email').keyup(function(){
+      $('.email').addClass('typing');
+      if( $(this).val().length == 0 ) {
+          $('.email').removeClass('typing');
+      }
+    });
+    $('#message').keyup(function(){
+      $('.message').addClass('typing');
+      if( $(this).val().length == 0 ) {
+          $('.message').removeClass('typing');
+      }
+    });
+     
 });
